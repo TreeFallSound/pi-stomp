56a03856e5ae467d52b61ae89c2bb380b365fb60
rreichenbach <randaji@gmail.com>
2021-08-10 04:24:38 +0100

    add -y to numpy install

--- setup/pkgs/simple_install.sh
+++ setup/pkgs/simple_install.sh
@@ -34,7 +34,7 @@ sudo /usr/bin/pip3 install RPi.GPIO
 
 # LCD
 sudo /usr/bin/pip3 install adafruit-circuitpython-rgb-display
-sudo apt install python3-numpy
+sudo apt install -y python3-numpy
 
 # MCP3xxx (ADC support)
 pushd $(mktemp -d) && curl https://files.pythonhosted.org/packages/57/3a/2d62e66b60619d6f15a2ebf08ad77fcc4196c924e489ec22b66e1977d88b/adafruit-circuitpython-mcp3xxx-1.4.1.tar.gz > mcp.tgz
