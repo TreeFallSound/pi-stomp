128a1dcaade76a02c82aae2e0058e989faaddead
DivideCo <micahvdm@gmail.com>
2021-11-06 21:58:44 +0200

    Simple update script
    This is just a base script to update and git pull. pipreq can be added at the end for checking against a list of requirements.

---
+++ modalapi/update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+#Script to update and check if necessary software additions are installed.
+
+#First up is to apt-get update and fix the bulleye bugs
+sudo apt-get update --allow-releaseinfo-change --fix-missing
+
+#Change directory to pi-stomp and pull latest updates from github
+cd ~patch/pi-stomp
+git pull
+
+#Additional software that gets added should be added here as an install, even if it's not seen as required.
+#This prevents things from breaking for current users who are updating.
