4392d8c276953ad07cc253d4dd84e90850e1762d
DivideCo <micahvdm@gmail.com>
2022-06-06 12:09:02 +0200

    Remove RT kernel stuff

--- setup/mod/install.sh
+++ setup/mod/install.sh
@@ -90,25 +90,3 @@ sudo cp hotspot/etc/dnsmasq.d/wifi-hotspot.conf /etc/dnsmasq.d
 sudo cp hotspot/etc/hostapd/hostapd.conf /etc/hostapd
 sudo cp -R hotspot/usr/lib/pistomp-wifi /usr/lib
 sudo cp hotspot/usr/lib/systemd/system/wifi-hotspot.service /usr/lib/systemd/system
-
-#RT Kernel install
-
-sudo dpkg -i linux-image-5.15.12-rt25-v8+_5.15.12-1_arm64.deb
-
-KERN=5.15.12-rt25-v8+
-sudo mkdir -p /boot/$KERN/overlays/
-sudo cp -d /usr/lib/linux-image-$KERN/overlays/* /boot/$KERN/o/
-sudo cp -dr /usr/lib/linux-image-$KERN/* /boot/$KERN/
-sudo touch /boot/$KERN/o/README
-sudo mv /boot/vmlinuz-$KERN /boot/$KERN/
-sudo mv /boot/System.map-$KERN /boot/$KERN/
-sudo cp /boot/config-$KERN /boot/$KERN/
-sudo cat >> /boot/config.txt << EOF
-
-[all]
-kernel=vmlinuz-$KERN
-#initramfs initrd.img-$KERN
-os_prefix=$KERN/
-overlay_prefix=o/
-[all]
-EOF
