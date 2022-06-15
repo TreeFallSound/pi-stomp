8a29b8589036808922741b0e96623392e84cb2e6
DivideCo <micahvdm@gmail.com>
2022-06-15 08:45:17 +0200

    Create rtkernel.sh

---
+++ setup/mod/rtkernel.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -x
+set -e
+
+sudo dpkg -i linux-image-5.15.12-rt25-v8+_5.15.12-1_arm64.deb
+
+KERN=5.15.12-rt25-v8+
+sudo mkdir -p /boot/$KERN/o/
+sudo cp -d /usr/lib/linux-image-$KERN/overlays/* /boot/$KERN/o/
+sudo cp -dr /usr/lib/linux-image-$KERN/* /boot/$KERN/
+sudo touch /boot/$KERN/overlays/README
+sudo mv /boot/vmlinuz-$KERN /boot/$KERN/
+sudo mv /boot/System.map-$KERN /boot/$KERN/
+sudo cp /boot/config-$KERN /boot/$KERN/
+sudo cat >> /boot/config.txt << EOF
+
+[all]
+kernel=vmlinuz-$KERN
+# initramfs initrd.img-$KERN
+os_prefix=$KERN/
+overlay_prefix=o/
+[all]
+EOF
