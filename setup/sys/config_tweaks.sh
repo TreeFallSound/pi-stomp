b3c5a57811ee240bef0d6d9e958befa554b4f2d1
DivideCo <micahvdm@gmail.com>
2022-06-17 19:56:05 +0200

    Update config_tweaks.sh

--- setup/sys/config_tweaks.sh
+++ setup/sys/config_tweaks.sh
@@ -26,7 +26,7 @@ sudo bash -c "sed -i \"s/^\s*display_auto_detect=/#display_auto_detect=/\" /boot
 sudo bash -c "sed -i \"s/^\s*dtoverlay=vc4-kms-v3d/#dtoverlay=vc4-kms-v3d/\" /boot/config.txt"
 
 # Enable SPI
-sudo bash -c “sed -i \“s/^\s*#dtparam=spi=on/dtparam=spi=on/\” /boot/config.txt”
+sudo bash -c "sed -i \"s/^\s*#dtparam=spi=on/dtparam=spi=on/\" /boot/config.txt"
 
 # append lines to config.txt
 cnt=$(grep -c "dtoverlay=pi3-disable-bt" /boot/config.txt)
