8f01908e616413c7ac8f29b9647ec13e0905e10f
DivideCo <micahvdm@gmail.com>
2022-06-18 19:29:21 +0200

    fixes and new rtk folder

--- setup/mod/rtkernel.sh
+++ setup/mod/rtkernel.sh
@@ -6,23 +6,23 @@ set -e
 sudo dpkg -i $HOME/pi-stomp/setup/mod/linux-image-5.15.12-rt25-v8+_5.15.12-1_arm64.deb
 
 KERN=5.15.12-rt25-v8+
-sudo mkdir -p /boot/$KERN/o/
-sudo cp -d /usr/lib/linux-image-$KERN/overlays/* /boot/$KERN/o/
-sudo cp -dr /usr/lib/linux-image-$KERN/* /boot/$KERN/
-sudo touch /boot/$KERN/overlays/README
-sudo mv /boot/vmlinuz-$KERN /boot/$KERN/
-sudo mv /boot/System.map-$KERN /boot/$KERN/
-sudo cp /boot/config-$KERN /boot/$KERN/
-sudo cat >> /boot/config.txt << EOF
+sudo mkdir -p /boot/rtk/o/
+sudo cp -d /usr/lib/linux-image-$KERN/overlays/* /boot/rtk/o/
+sudo cp -dr /usr/lib/linux-image-$KERN/* /boot/rtk/
+sudo touch /boot/rtk/o/README
+sudo mv /boot/vmlinuz-$KERN /boot/rtk/
+sudo mv /boot/System.map-$KERN /boot/rtk/
+sudo cp /boot/config-$KERN /boot/rtk/
+sudo bash -c "cat >> /boot/config.txt << EOF
 
 [all]
 kernel=vmlinuz-$KERN
 # initramfs initrd.img-$KERN
-os_prefix=$KERN/
+os_prefix=rtk/
 overlay_prefix=o/
 [all]
-EOF
+EOF"
 
 #Turn off raspi-config service and set performance governor
 sudo rcconf --off raspi-config
-sudo "echo performance > /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor"
+sudo bash -c "echo performance | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor"
