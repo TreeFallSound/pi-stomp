076029b633c1d05aba3c9bfcbdf52152eedc26ef
DivideCo <micahvdm@gmail.com>
2022-06-25 21:22:29 +0200

    changes to jackdrc not needed

--- setup/audio/audiocard-setup.sh
+++ setup/audio/audiocard-setup.sh
@@ -31,7 +31,3 @@ dtoverlay=audioinjector-wm8731-audio
 EOF"
 fi
 
-# Change jack config to use card 0
-sudo sed -i -e 's/hw:pisound/hw:0/g' /etc/jackdrc
-sudo sed -i -e 's/-p 128/-p 256/' /etc/jackdrc
-
