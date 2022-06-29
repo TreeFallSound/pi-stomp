87e140b05acffe1ff5004abd385456b7ee32f65d
DivideCo <micahvdm@gmail.com>
2022-06-29 09:05:27 +0200

    uncomment

--- setup/pkgs/gfxhat_install.sh
+++ setup/pkgs/gfxhat_install.sh
@@ -16,9 +16,9 @@
 # along with pi-stomp.  If not, see <https://www.gnu.org/licenses/>.
 
 # GFXHat LCD
-#pushd $(mktemp -d)
-#curl -k https://get.pimoroni.com/gfxhat > gfxhat.sh
-#sed -i 's/pip2support="yes"/pip2support="no"/' gfxhat.sh
-#chmod a+x gfxhat.sh
-#./gfxhat.sh -y
+pushd $(mktemp -d)
+curl -k https://get.pimoroni.com/gfxhat > gfxhat.sh
+sed -i 's/pip2support="yes"/pip2support="no"/' gfxhat.sh
+chmod a+x gfxhat.sh
+./gfxhat.sh -y
 
