c76ad2542016445c8b66d45db81ea20435d27304
DivideCo <micahvdm@gmail.com>
2022-08-07 10:23:47 +0200

    incorrect path fix

--- setup/sys/rtkernel.sh
+++ setup/sys/rtkernel.sh
@@ -30,7 +30,7 @@ sudo mv /boot/vmlinuz-$KERN /boot/$KERN/
 sudo mv /boot/initrd.img-$KERN /boot/$KERN/
 sudo mv /boot/System.map-$KERN /boot/$KERN/
 sudo cp /boot/config-$KERN /boot/$KERN/
-sudo cp iqaudio-codec.dtbo /boot/$KERN/o/
+sudo cp /home/pistomp/pi-stomp/setup/sys/iqaudio-codec.dtbo /boot/$KERN/o/
 sudo bash -c "cat >> /boot/config.txt << EOF
 [all]
 kernel=vmlinuz-$KERN
