84b21102bec10227fc18e491018a37760b05419d
Micah Van de Merwe <micahvdm@Micahs-MacBook-Pro.local>
2023-04-04 08:36:41 +0200

    update some more config locations missed

--- util/update-menu.sh
+++ util/update-menu.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-mkdir -p /home/pistomp/data/configs
+Sudo apt install -y lockfile-progs
+
+mkdir -p /home/pistomp/data/config_templates
 mkdir -p '/home/pistomp/data/user-files/Aida DSP Models'
 
 ln -s /home/pistomp/data/.pedalboards /home/pistomp/.pedalboards
 ln -s /home/pistomp/.lv2 /home/pistomp/data/.lv2
 
 #move default config files to data dir
-cd /home/pistomp/pi-stomp/pistomp
-cp default* /home/pistomp/data/configs
+cp /home/pistomp/pi-stomp/setup/config_templates/default* /home/pistomp/data/config_templates
 
 #USB automounter
 sudo dpkg -i /home/pistomp/pi-stomp/setup/mod/usbmount.deb
