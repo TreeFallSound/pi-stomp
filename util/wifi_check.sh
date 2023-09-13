34be76d00fb324ac8f3159e20e1c49a8203ef84b
Micah van de Merwe <micahvdm@gmail.com>
2023-09-13 22:46:02 +0200

    unnecessary to enable and disable service

--- util/wifi_check.sh
+++ util/wifi_check.sh
@@ -5,10 +5,8 @@ LOG="/home/pistomp/wifi_check.log"
 iwgetid -r &>/dev/null
 
 if [ $? -eq 0 ]; then
-    sudo systemctl disable wifi-hotspot.service
     echo "Wifi is connected." >> "$LOG"
 else
-    sudo systemctl enable wifi-hotspot.service
     sudo systemctl start wifi-hotspot.service
     echo "Wifi not connected. Starting hotspot." >> "$LOG"
 fi
