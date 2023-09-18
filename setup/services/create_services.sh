28fd4cc6f9f826b0fd4c0d9857e05228791756db
DivideCo <micahvdm@gmail.com>
2023-09-18 23:35:57 +0200

    fix path

--- setup/services/create_services.sh
+++ setup/services/create_services.sh
@@ -39,4 +39,4 @@ sudo dpkg -i setup/services/usbmount.deb
 sudo cp setup/services/wifi_check.sh /etc/wpa_supplicant/
 
 # Copy wlan0.conf to prevent wifi power save mode
-sudo cp wlan0.conf /etc/network/interfaces.d/
+sudo cp setup/services/wlan0.conf /etc/network/interfaces.d/
